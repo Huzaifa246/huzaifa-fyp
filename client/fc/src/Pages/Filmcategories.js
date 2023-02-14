12e484c4e7db8144d601609018f69cb2113bbe74
Huzaifa246 <Huzaifa24680@gmail.com>
2023-02-14 11:48:19 -0800

    categories

--- client/fc/src/Pages/Filmcategories.js
+++ client/fc/src/Pages/Filmcategories.js
@@ -1,43 +1,19 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import { React, Card, CardContent, CardMedia, Typography, Grid } from "./imports";
+import { useEffect, useState } from "react"
+import axios from "axios";
 
-const sessionsData = [
-    {
-        Image: "https://i.tribune.com.pk/media/images/2238264-vasayy-1591614759/2238264-vasayy-1591614759.jpg",
-        Name: "Vasay Chaudhry",
-        desc: " Pakistani screenwriter, actor, director, producer, host, and comedian"
-    },
-    {
-        Image: "https://i0.wp.com/www.primesworld.com/wp-content/uploads/Sarwat-Gilani-Biography-Height-Age-TV-Serials-Husband-Family-Salary-Net-Worth-Awards-Photos-Facts-More.jpg?resize=1600%2C960&ssl=1",
-        Name: "Sarwat Gilani",
-        desc: "Pakistani model, film, television and voice actress."
-    },
-    {
-        Image: "https://pakistani.pk/uploads/reviews/photos/thumbnail/200x200c/9a/77/a9/ashrafbilal-50883753-996608377195067-6960332599117679267-n-77-1554108773.jpg",
-        Name: "Bilal Ashraf",
-        desc: "Pakistani film actor and visual effects director"
-    },
-    {
-        Image: "https://i.dawn.com/large/2021/03/605b0341bb22a.jpg",
-        Name: "Kubra Khan",
-        desc: "Film and Tv actress of Pakistani industry"
-    },
-    {
-        Image: "https://i.tribune.com.pk/media/images/Ahmed-Butt-weight-loss1611046025-0/Ahmed-Butt-weight-loss1611046025-0.jpg",
-        Name: "Ahmed Ali Butt",
-        desc: "Pakistani actor, television host, comedian, singer and songwriter"
-    },
-    {
-        Image: "https://i.tribune.com.pk/media/images/2234440-syra-1591176131/2234440-syra-1591176131.jpg",
-        Name: "Syra Yousuf",
-        desc: "Pakistani model, tv actress and former VJ"
-    },
-];
+
 export default function CategoriesCard() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const func = async () => {
+            await axios.get("http://localhost:5000/api/celebs").then((resp) => {
+                return setData(resp.data.celebrities);
+            });
+        };
+        func();
+    }, []);
+
     return (
         <>
             <div class="section-header" style={{ height: "50px", marginTop: "40px", marginLeft: "90px" }}>
@@ -45,30 +21,32 @@ export default function CategoriesCard() {
             </div>
 
             <Grid container >
-                {/* {Array.from(Array(6)).map((_, index) => ( */}
-                {sessionsData.map((index) => (
-                    <Grid item xs={4} m="auto" pt={3} px={3} key={index} align='center' padding="90px" >
-                        <Card sx={{ maxWidth: 500, maxHeight: 'auto', marginTop: "-60px" }}>
-                            <CardMedia
-                                sx={{ width: 500, height: 250 }}
-                                component="img"
-                                height="120"
-                                image={index.Image}
-                                alt="green iguana"
+                {data.filter((index) => index.category === "film")
+                    .map((index) => {
+                        return (
+                            <Grid item xs={4} m="auto" pt={3} px={3} key={index} align='center' padding="90px" >
+                                <Card sx={{ maxWidth: 500, maxHeight: 'auto', marginTop: "-60px" }}>
+                                    <CardMedia
+                                        sx={{ width: 500, maxHeight: 450, minHeight: 250 }}
+                                        component="img"
+                                        height="120"
+                                        image={index.image}
+                                        alt="Film Stars"
 
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {index.Name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {index.desc}
-                                </Typography>
-                            </CardContent>
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {index.name}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {index.bio}
+                                        </Typography>
+                                    </CardContent>
 
-                        </Card>
-                    </Grid>
-                ))}
+                                </Card>
+                            </Grid>
+                        );
+                    })}
             </Grid>
         </>
     );
--- client/fc/src/Pages/YTcategories.js
+++ client/fc/src/Pages/YTcategories.js
@@ -1,42 +1,17 @@
 import { React, Card, CardContent, CardMedia, Typography, Grid } from "./imports";
-const sessionsData = [
-  {
-    Image: "https://hamariweb.com/profiles/images/profile/4347-679.jpg",
-    Name: "Junaid Akram",
-    desc: "Youtuber, Blogger, Podcasts Host discuss Culture and Current Affairs",
-  },
-  {
-    Image:
-      "https://1.bp.blogspot.com/-qhxsmeZpsNk/YAHF_iqFGhI/AAAAAAAABkg/dJOHnSX10i0FDSLcuEv21pVJS9pkv2NmgCLcBGAsYHQ/s577/ukhano.png",
-    Name: "Umer Khan",
-    desc: "Pakistani YouTuber, Vlogger, Photographer and Traveler",
-  },
-  {
-    Image:
-      "https://pakistantime.net/wp-content/uploads/2021/05/Couple-Of-The-Year-Looks-Like-Ducky-Bhai-Sham.jpg",
-    Name: "Saad ur Rehman",
-    desc: "Pakistani YouTuber, Gamer, and Roaster. Famous as Ducky bhai",
-  },
-  {
-    Image:
-      "https://pakistani.pk/uploads/reviews/photos/thumbnail/1500x500s/9c/72/36/Taimoor-Salahuddin-Complete-Information-53-1599593719.jpg",
-    Name: "Taimoor Salahuddin",
-    desc: "YouTuber, Influencer, Filmmaker , Actor, Comedian and Musician.",
-  },
-  {
-    Image:
-      "https://1.bp.blogspot.com/-gRtDv6fznk8/XooCwWdKA0I/AAAAAAAACgI/IlP-EtIWwLslpm2-6g0w18JDFJkv2nAZgCLcBGAsYHQ/s1600/1623926536934406-c5-1080x1080.jpg",
-    Name: "Shahveer Jafry",
-    desc: "Pakistani Youtuber, Blogger and Sketch-Comedy artist",
-  },
-  // {
-  //     id: 6,
-  //     url: "",
-  //     title: "",
-  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nisi accusamus quos molestias!"
-  // },
-];
+import { useEffect, useState } from "react"
+import axios from "axios";
+
 export default function CategoriesCard() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const func = async () => {
+      await axios.get("http://localhost:5000/api/celebs").then((resp) => {
+        return setData(resp.data.celebrities);
+      });
+    };
+    func();
+  }, []);
   return (
     <>
       <div
@@ -46,38 +21,40 @@ export default function CategoriesCard() {
         Youtubers
       </div>
       <Grid container>
-        {/* {Array.from(Array(6)).map((_, index) => ( */}
-        {sessionsData.map((index) => (
-          <Grid
-            item
-            xs={4}
-            m="auto"
-            pt={3}
-            px={3}
-            key={index}
-            align="center"
-            padding="90px"
-            marginTop="-60px"
-          >
-            <Card sx={{ maxWidth: 500, maxHeight: "auto" }}>
-              <CardMedia
-                sx={{ width: 500, height: 250 }}
-                component="img"
-                height="120"
-                image={index.Image}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {index.Name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {index.desc}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+        {data.filter((index) => index.category === "youtuber")
+          .map((index) => {
+            return (
+              <Grid
+                item
+                sm={4}
+                m="auto"
+                pt={3}
+                px={3}
+                key={index}
+                align="center"
+                padding="90px"
+                marginTop="-60px"
+              >
+                <Card sx={{ maxWidth: 500, maxHeight: "auto" }}>
+                  <CardMedia
+                    sx={{ width: 500, maxHeight: 450, minHeight: 300 }}
+                    component="img"
+                    height="120"
+                    image={index.image}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {index.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {index.bio}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
       </Grid>
     </>
   );
