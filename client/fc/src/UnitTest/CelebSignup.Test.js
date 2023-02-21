4002a232af9aec3814c95d2e65bcbf1cb411de4f
Huzaifa246 <Huzaifa24680@gmail.com>
2023-02-21 10:31:34 -0800

    a

--- client/fc/src/UnitTest/CelebSignup.Test.js
+++ client/fc/src/UnitTest/CelebSignup.Test.js
@@ -2,25 +2,14 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import CelebSignup from "../Pages/CelebSignup";
 import Signup from './../Pages/CelebSignup';
 
-//  const FormTestDetails = () => {
-//    // const inputs = document.querySelectorAll('input');
-//    // const loginInput = inputs[0];
-// }
-
 test("Signup when Full Name , Email and Password are entered correctly", () => {
-   render(<CelebSignup Signup={Signup}/>);
+   render(<CelebSignup Signup={Signup} />);
 
    const inputs = document.querySelectorAll('input');
    const slug = inputs[0];
    const email = inputs[1];
    const password = inputs[2];
    const button = inputs[3];
-   
-   // const slug = screen.input("username");
-   // const email = screen.input("Email");
-   // const password = screen.input("password");
-   // const button = screen.getByRole("button");
-
 
    fireEvent.change(slug, { target: { value: "Huzaifa Rehman" } })
    fireEvent.change(email, { target: { value: "huzaifa@gmail.com" } })
